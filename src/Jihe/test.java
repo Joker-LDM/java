1707159d1250f3949bcebf41fceed2f6c99225f6
Administrator <2509657914@qq.cOM>
2021-01-11 18:15:53 +0800

    集合中的list的基本操作

---
+++ src/Jihe/test.java
@@ -0,0 +1,18 @@
+package Jihe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+    public static void main(String[] args) {
+        List list =new ArrayList();
+        list.add(1);
+        list.add("河池学院");
+
+
+    for (Object Lst:list)
+        System.out.println("当前集合的元素个数："+Lst);
+
+    }
+
+}
